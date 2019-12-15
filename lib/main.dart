57a33ca465d41fbcf24f330442df05535efc4699
CodeSadhu <patwardhanatharva6@gmail.com>
2019-12-15 13:08:58 +0530

    Heart Icons added
    Non - Responsive Heart icons added.

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,52 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> {
 	@override
 		Widget build(BuildContext context) {
+<<<<<<< Updated upstream
 			// TODO: implement build
 			final wordPair = WordPair.random();
 			return Text(wordPair.asPascalCase);
+=======
+			final List<WordPair> _suggest = <WordPair>[];
+			final Set<WordPair> _saved = Set<WordPair>();
+			final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
+
+			Widget _buildRow(WordPair pair) {
+
+				final bool alreadySaved = _saved.contains(pair);
+
+				return ListTile(
+					title: Text(
+						pair.asPascalCase,
+						style: _biggerFont,
+						),
+						trailing: Icon(
+							alreadySaved ? Icons.favorite : Icons.favorite_border,
+							color: alreadySaved ? Colors.red: null,
+						),
+				);
+			}
+
+			Widget _buildSuggestions() {
+				return ListView.builder(
+					padding: const EdgeInsets.all(16.0),
+					itemBuilder: (context, i) {
+						if (i.isOdd) return Divider();
+
+						final index = i ~/2;	//return i/2 and convert to integer. For eg. 3/2 = 1
+						if (index >= _suggest.length) {
+							_suggest.addAll(generateWordPairs().take(10));
+						}
+						return _buildRow(_suggest[index]);
+					});
+			}
+
+			return Scaffold(
+				appBar: AppBar(
+					title: Text('Startup Name Generator'),
+				),
+				body: _buildSuggestions(),
+			);
+>>>>>>> Stashed changes
 		}
 }
 
