29958439fc45ba9d4e20c7fd6611787ee11aa018
CodeSadhu <patwardhanatharva6@gmail.com>
2020-02-19 19:59:12 +0530

    Uploaded screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -4,77 +4,71 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-	// This widget is the root of your application.
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			debugShowCheckedModeBanner: false,
-			title: 'Welcome to Flutter!',
-			home: Scaffold(
-				appBar: AppBar(
-					title: Text('Welcome to Flutter!'),
-				),
-				body: Center(
-					child: RandomWords(),
-				),
-			),
-		);
-	}
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
 }
 
 class RandomWordsState extends State<RandomWords> {
-	@override
-		Widget build(BuildContext context) {
-<<<<<<< Updated upstream
-			// TODO: implement build
-			final wordPair = WordPair.random();
-			return Text(wordPair.asPascalCase);
-=======
-			final List<WordPair> _suggest = <WordPair>[];
-			final Set<WordPair> _saved = Set<WordPair>();
-			final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final _suggest = <WordPair>[];
+    final Set<WordPair> _savedWordPair = Set<WordPair>();
+    final _biggerFont = const TextStyle(fontSize: 18.0);
 
-			Widget _buildRow(WordPair pair) {
+    Widget _buildRow(WordPair pair) {
+      final bool alreadySaved = _savedWordPair.contains(pair);
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(alreadySaved ? Icons.favorite : 
+        Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPair.remove(pair);
+            } else {
+              _savedWordPair.add(pair);
+            }
+          });
+        },
+      );
+    }
 
-				final bool alreadySaved = _saved.contains(pair);
+    Widget _buildSuggestions() {
+      return ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return Divider();
 
-				return ListTile(
-					title: Text(
-						pair.asPascalCase,
-						style: _biggerFont,
-						),
-						trailing: Icon(
-							alreadySaved ? Icons.favorite : Icons.favorite_border,
-							color: alreadySaved ? Colors.red: null,
-						),
-				);
-			}
+            final index = i ~/ 2; //return i/2 and convert to integer. For eg. 3/2 = 1
+            if (index >= _suggest.length) {
+              _suggest.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggest[index]);
+          });
+    }
 
-			Widget _buildSuggestions() {
-				return ListView.builder(
-					padding: const EdgeInsets.all(16.0),
-					itemBuilder: (context, i) {
-						if (i.isOdd) return Divider();
-
-						final index = i ~/2;	//return i/2 and convert to integer. For eg. 3/2 = 1
-						if (index >= _suggest.length) {
-							_suggest.addAll(generateWordPairs().take(10));
-						}
-						return _buildRow(_suggest[index]);
-					});
-			}
-
-			return Scaffold(
-				appBar: AppBar(
-					title: Text('Startup Name Generator'),
-				),
-				body: _buildSuggestions(),
-			);
->>>>>>> Stashed changes
-		}
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 }
 
 class RandomWords extends StatefulWidget {
-	@override
-	RandomWordsState createState() => RandomWordsState();
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
